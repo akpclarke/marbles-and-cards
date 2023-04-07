30a7e8fb4cc65542f8e2ab2c40834cb62c896a62
akpclarke <andrew.kpclarke@gmail.com>
2023-04-07 01:31:03 -0400

    Initial project setup

---
+++ main.js
@@ -0,0 +1,15 @@
+import * as PIXI from './node_modules/pixi.js/dist/pixi.min.mjs';
+
+let app;
+
+window.onload = function() {
+    app = new PIXI.Application(
+        {
+            width: 800,
+            height: 600,
+            backgroundColor: 0xAAAAAA
+        }
+    );
+
+    document.body.appendChild(app.view);
+}
